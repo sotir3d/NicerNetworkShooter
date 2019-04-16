7e1fb5fe4f08cdc245692a88e9c2d17e10959dce
Sebastian Fülöp <sebastian.fueloep@gmail.com>
2019-04-16 16:00:53 +0200

    INIT

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D _rigidBody2D;
+
+    private void Start()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isLocalPlayer)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            //Vector3 movement = new Vector3(horizontal, vertical, 0f);
+
+            //transform.position += movement * Time.deltaTime * moveSpeed;
+
+            _rigidBody2D.velocity = new Vector3(horizontal, vertical, 0f) * Time.fixedDeltaTime * moveSpeed;
+        }
+    }
+}
